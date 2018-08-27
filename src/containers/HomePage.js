761488d47cb3a9d4f46d7b5452f4a4f0ee84fb19
jeffreysu1355 <jeffreysu1355@gmail.com>
2018-08-26 20:29:36 -0700

    Added react-typist

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Grid, Header, Container } from 'semantic-ui-react';
 import styled from 'styled-components';
 import img from '../images/computer-desk.jpg';
-import img2 from '../images/Random.png';
+import Typist from 'react-typist';
 
 class HomePage extends Component{
 
@@ -16,22 +16,27 @@ class HomePage extends Component{
             background-position: center center; 
             z-index: -1;
         `;
-        // const style={
-        //     height: "100vh",
-        //     background-image: url(${img});
-        //
-        // };
+
         return (
                 <Container fluid={true}>
                     <Content>
                         <Grid >
-                                <Grid.Row verticalAlign="middle" centered>
-                                    <Grid.Column>
-                                        <Header className="name-header">Jeffrey Su</Header>
-                                    </Grid.Column>
-                                </Grid.Row>
+                            <Grid.Row verticalAlign="middle" centered>
+                                <Grid.Column>
+                                    <Typist className="name-header">
+                                        {/*<Header className="name-header">Hi, I'm Jeffrey!</Header>*/}
+                                        Hi, I'm Jeffrey!
+                                    </Typist>
+                                </Grid.Column>
+                            </Grid.Row>
+                            <Grid.Row>
+                                <Grid.Column>
+
+                                </Grid.Column>
+                            </Grid.Row>
                         </Grid>
                     </Content>
+
 
                 </Container>
 
