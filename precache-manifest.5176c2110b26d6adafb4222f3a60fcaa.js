d50f0067a7dc89058ca386d4977d69d60fca3a06
jeffreysu1355 <jeffreysu1355@gmail.com>
2019-10-03 12:40:15 -0700

    New Website

---
+++ precache-manifest.5176c2110b26d6adafb4222f3a60fcaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a90d616f1d4e6a8e72a81ccc18e376e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9d742d9a17479f4a324",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3925a0886dac9b23ce16",
+    "url": "/static/js/2.a690b370.chunk.js"
+  },
+  {
+    "revision": "d9d742d9a17479f4a324",
+    "url": "/static/js/main.eb94c605.chunk.js"
+  },
+  {
+    "revision": "bfd1521e694d93483f19",
+    "url": "/static/js/runtime-main.14a2f347.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
